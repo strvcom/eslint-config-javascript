2d0263f6d3ad81eb9f9b8b216297836649b7733d
Robert Rossmann <rr.rossmann@me.com>
2016-01-24 10:38:50 +0100

    Proper file headers

--- environments/nodejs/latest.js
+++ environments/nodejs/latest.js
@@ -1,3 +1,11 @@
+/**
+ * js-coding-standards
+ *
+ * @author      Robert Rossmann <rr.rossmann@me.com>
+ * @copyright   2016 STRV
+ * @license     http://choosealicense.com/licenses/bsd-3-clause  BSD-3-Clause License
+ */
+
 'use strict'
 
 module.exports = {
