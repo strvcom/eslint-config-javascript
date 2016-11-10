2a087f35c5ed23ab508e1f278689a4c879115608
Robert Rossmann <rr.rossmann@me.com>
2016-11-10 16:29:54 +0100

    Add config file for Node.js 7 release

---
+++ environments/nodejs/v7.js
@@ -0,0 +1,18 @@
+/**
+ * js-coding-standards
+ *
+ * @author      Robert Rossmann <rr.rossmann@me.com>
+ * @copyright   2016 STRV
+ * @license     http://choosealicense.com/licenses/bsd-3-clause  BSD-3-Clause License
+ */
+
+'use strict'
+
+/**
+ * This file configures ESLint to work with the Node.js 7.0 release
+ */
+module.exports = {
+
+  // There is no difference in terms of syntax or ES level support in v7 compared to v6
+  extends: './v6.js',
+}
