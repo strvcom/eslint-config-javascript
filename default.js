a45d277c0ec83930f1ca1697a3a0485b9d55f189
Robert Rossmann <rr.rossmann@me.com>
2017-08-29 16:41:08 +0200

    Add @TODO to default ruleset

--- default.js
+++ default.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   extends: [
+    // @TODO(breaking): Bump to latest node.js ruleset on next major release
     './environments/nodejs/v6.js',
     './environments/nodejs/optional.js',
     './coding-styles/recommended.js',
