ca112eff0e2b5fae786e92af1a95bd308044ba7e
Petr Hanák <petr.hanak@strv.com>
2017-11-17 18:53:03 +0100

    Add react/no-unescaped-entities

--- environments/react/optional.js
+++ environments/react/optional.js
@@ -31,5 +31,10 @@ module.exports = {
     // babel-plugin-transform-react-remove-prop-types to remove propTypes from their components in
     // production builds, to do so safely
     'react/forbid-foreign-prop-types': 'warn',
+
+    // This rule prevents characters that you may have meant as JSX escape characters from being
+    // accidentally injected as a text node in JSX statements.
+    // Also breaks syntax highlighting in github pull requests.
+    'react/no-unescaped-entities': 'warn',
   },
 }
