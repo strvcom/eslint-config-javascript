00bcd8bb941d7d907e9aa2c69ac8d8582296be50
Robert Rossmann <robert.rossmann@strv.com>
2019-02-01 18:23:06 +0100

    fix: only install git hooks from inside this project's git root
    This fixes a problem where running `npm i strvcom/eslint-plugin-javascript` would fail due to the _install-githooks.sh_ script not being able to find the git hooks.

--- utils/install-githooks.sh
+++ utils/install-githooks.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-echo "Installing git hooks:"
-echo utils/githooks/*
+# Only install git hooks if we are in the project's git root
+# Do NOT install git hooks when ie. installing this package using npm directly from Github
+if [ -d "utils/githooks" ]; then
+  echo "Installing git hooks:"
+  echo utils/githooks/*
 
-cp -r utils/githooks/* .git/hooks/
+  cp -r utils/githooks/* .git/hooks/
+fi
